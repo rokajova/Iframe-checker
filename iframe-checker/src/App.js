79eca6973b7b8a167f8503736e22ad4e1345c4ee
rokajova <35878122+rokajova@users.noreply.github.com>
2021-04-20 11:17:35 +0300

    .

--- iframe-checker/src/App.js
+++ iframe-checker/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
+
 function App() {
+  const [iframeConnected, setIframeConnected] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Enter a link to check..." autoFocus required />
+      <button onClick={ () =>setIframeConnected (!iframeConnected)}>Check!</button>
+      {iframeConnected ? <div>yes!</div> :<div>no!</div> }
+    
     </div>
   );
 }
