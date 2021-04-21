25491db627779d76c2db8d74b5de835be772122c
rokajova <35878122+rokajova@users.noreply.github.com>
2021-04-21 10:07:53 +0300

    iframechecker

--- iframe-checker/src/App.js
+++ iframe-checker/src/App.js
@@ -1,16 +1,33 @@
-import React, {useState} from "react"
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [iframeConnected, setIframeConnected] = useState(false)
+  const [iframeLink, setIframeLink] = useState();
+  const [checkIframe, setCheckIframe] = useState();
+
+  // Check is string is an URL
+  function is_url(str) {
+    let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   return (
     <div className="App">
-      <input type="text" placeholder="Enter a link to check..." autoFocus required />
-      <button onClick={ () =>setIframeConnected (!iframeConnected)}>Check!</button>
-      {iframeConnected ? <div>yes!</div> :<div>no!</div> }
-    
+      <input
+        type="text"
+        placeholder="Enter a link to check..."
+        autoFocus
+        required
+        value={iframeLink}
+        onChange={(e) => setIframeLink(e.target.value)}
+      />
+      <button onClick={() => setCheckIframe(iframeLink)}>CHECK</button>
+
+      <iframe src={checkIframe} />
     </div>
   );
 }
